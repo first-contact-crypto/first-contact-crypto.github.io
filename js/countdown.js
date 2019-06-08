6eba832107d730b0d258e1362245309be6df15ea
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-07 19:51:16 -0600

    countdown timer

---
+++ js/countdown.js
@@ -0,0 +1,37 @@
+// Set the date we're counting down to
+var keepkeyCountDownDate = new Date("June 16, 2019 23:59:59").getTime()
+var bitcoin5CountDownDate = new Date("June 16, 2019 23:59:59").getTime()
+var amazon5CountDownDate = new Date("June 16, 2019 23:59:59").getTime()
+
+// Update the count down every 1 second
+function mySetInterval(countDownDate) {
+    var x = setInterval(function() {
+    // Get today's date and time
+        var now = new Date().getTime()
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            // eslint-disable-next-line indent
+      document.getElementById("demo").innerHTML = "EXPIRED"
+        }
+    }, 1000)
+}
+
+
+mySetInterval(keepkeyCountDownDate)
+mySetInterval(bitcoin5CountDownDate)
+mySetInterval(amazon5CountDownDate)
