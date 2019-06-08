7fbe1911c898a4bd4ef0699fe431b22995fb3527
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-08 03:46:24 -0600

    coundown timer again 2

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@ var bitcoin5CountDownDate = new Date("June 16, 2019 23:59:59").getTime()
 var amazon5CountDownDate = new Date("June 16, 2019 23:59:59").getTime()
 
 // Update the count down every 1 second
-function mySetInterval(countDownDate) {
+function mySetInterval(el_id, countDownDate) {
     var x = setInterval(function() {
     // Get today's date and time
         var now = new Date().getTime()
@@ -19,21 +19,23 @@ function mySetInterval(countDownDate) {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
         // Display the result in the element with id="demo"
-        document.getElementById("countdown-timer-keepkey").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
-        document.getElementById("countdown-timer-bitcoin5").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
-        document.getElementById("countdown-timer-amazon5").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+        // document.getElementById("countdown-timer-keepkey").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+        // document.getElementById("countdown-timer-bitcoin5").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+        // document.getElementById("countdown-timer-amazon5").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+        document.getElementById(el_id).innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
         // If the count down is finished, write some text
         if (distance < 0) {
-            clearInterval(x)
-            // eslint-disable-next-line indent
-            document.getElementById("countdown-timer-keepkey").innerHTML = "EXPIRED"
-            document.getElementById("countdown-timer-bitcoin5").innerHTML = "EXPIRED"
-            document.getElementById("countdown-timer-amazon5").innerHTML = "EXPIRED"
-        }
+                            clearInterval(x);
+                            // eslint-disable-next-line indent
+                            // document.getElementById("countdown-timer-keepkey").innerHTML = "EXPIRED"
+                            // document.getElementById("countdown-timer-bitcoin5").innerHTML = "EXPIRED"
+                            // document.getElementById("countdown-timer-amazon5").innerHTML = "EXPIRED"
+                            document.getElementById(el_id).innerHTML = "EXPIRED"
+                          }
     }, 1000)
 }
 
 
-mySetInterval(bitcoin5CountDownDate)
-// mySetInterval("countdown-timer-bitcoin5",)
-// mySetInterval("countdown-timer-amazon5", amazon5CountDownDate)
+mySetInterval("countdown-timer-keepkey", keepkeyCountDownDate)
+mySetInterval("countdown-timer-bitcoin5",bitcoin5CountDownDate)
+mySetInterval("countdown-timer-amazon5", amazon5CountDownDate)
