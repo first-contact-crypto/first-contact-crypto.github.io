1ef47ba2c5bada29b6f32480c4ccaa1fd4c090fa
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-10 12:00:21 -0600

    added github api 5

--- js/redemption.js
+++ js/redemption.js
@@ -537,13 +537,18 @@ function prizeAccounting() {
     contentType: "application/json",
     timeout: 3000,
     url: url,
-    success: successfunc,
-    error: errorfunc,
+    success: function(data, status, jqXhr) {
+      window.assertions = data;
+      window.num_epiph_asserts = assertions.result.length;
+      PRINT("SUCCESS: In prizeAccounting.. data: {0}", data);
+    },
+    error: function(jqXhr, textStatus, errorMessage) {
+      PRINT("ERROR: In prizeAccounting.. {0}, {1}", textStatus, errorMessage);
+    },
     beforeSend: function(xhr) {
       // xhr.setRequestHeader("Authorization", "Bearer " + BADGR_ACCESS_TOKEN)
       // xhr.setRequestHeader("Content-Type", "application/json")
-      xhr.setRequestHeader("User-Agent", "First-Contact-Crypto")
-
+      xhr.setRequestHeader("User-Agent", "First-Contact-Crypto");
     }
   });
 }
