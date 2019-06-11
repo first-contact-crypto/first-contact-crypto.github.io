c4131fd3f261111fc495a61edbe345f1739b09c6
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-10 19:04:53 -0600

    prizeAccounting 11

--- js/redemption.js
+++ js/redemption.js
@@ -558,7 +558,7 @@ function prizeAccounting() {
     epSpent.email = useremail
     epSpent.prize = selectedPrize
     epSpent.numEPSpent = ep_spent
-    epSpent.timestamp = Date.getTime();
+    epSpent.timestamp = Date.now();
     bp.extensions = epSpent
     $.ajax({
       method: "POST",
