71299d62e790804af606e6952be91e0a232fa3fc
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-07-19 06:57:42 -0600

    bat

--- js/redemption.js
+++ js/redemption.js
@@ -1,6 +1,6 @@
 const DEV_ENV = false;
 
-const BAT = "NGqc7IvF5YD1wA7aDPdsAwFx4rXFAi";
+const BAT = "oa519sWUya03Bynu7aq7FWYiOpKdii";
 const BADGR_ISSUER_ID = "MC67oN42TPm9VARGW7TmKw";
 const BADGR_COURSE_TYPE = "course";
 const BADGR_EPIPHANY_TYPE = "epiphany";
