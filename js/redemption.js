dc6690ddc88a8207aec16cfb564dca549ec5408e
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-10-03 19:51:33 -0600

    added-log-statement-2

--- js/redemption.js
+++ js/redemption.js
@@ -222,7 +222,7 @@ function getAssertions() {
   for (i = 0; i < num_assertions_before; ++i) {
     a = assertions_list[i];
     PRINT(
-      "INFO: In getAssertions.. a.recipient.identity: {} .. window.useremail: {}".format(a.recipient.identity, window.useremail)
+      "INFO: In getAssertions.. a.recipient.identity: {} .. window.useremail: {}", a.recipient.identity, window.useremail
     );
     if (a.recipient.identity === window.useremail && a.revoked === false) {
       assertions_to_keep.push(a);
